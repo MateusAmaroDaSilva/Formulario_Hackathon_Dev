f3fc4c67d1d1c18fda09f3008e74cb9529943f08
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2025-12-18 13:14:40 -0300

    Feat: Implementa título dinâmico na aba do navegador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,3 +11,19 @@
     @yield('content')
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>@yield('title', 'DevMenthors')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.ico') }}">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
